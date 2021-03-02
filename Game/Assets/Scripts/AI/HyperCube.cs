e190f8d0493a344f7fbd4c949ae262d8c2e79d3a
aburch13 <ab13189@gmail.com>
2021-03-02 14:29:38 -0500

    Hyper cube v3

--- Game/Assets/Scripts/AI/HyperCube.cs
+++ Game/Assets/Scripts/AI/HyperCube.cs
@@ -6,8 +6,11 @@ public class HyperCube : Controller
 {
     public GameObject targ;
     public float burst = 2;
+    public float speed = 25;
+    public GameObject parent;
     private float localTime;
     private Vector3 last;
+    private Vector3 offset;
     private bool go = false;
 
     public override void setTime(float f)
@@ -24,6 +27,9 @@ public class HyperCube : Controller
     // Update is called once per frame
     void Update()
     {
+        if(localTime != 0)
+        transform.LookAt(targ.transform);
+        //Use for pyramid
         if (burst < -1)
         {
             go = false;
@@ -32,12 +38,33 @@ public class HyperCube : Controller
         }
         else if (burst < 0 && !go)
         {
+            last = targ.transform.position + Vector3.up + offset;
             burst -= Time.deltaTime * localTime;
-            last = targ.transform.position;
-            GetComponent<Rigidbody>().velocity = Vector3.Normalize(transform.position - last) * -25;
+            GetComponent<Rigidbody>().velocity = (Vector3.Normalize(last - transform.position)) * speed * localTime;
             go = true;
         }
-        else
+        else if (go)
+        {
             burst -= Time.deltaTime * localTime;
+        }
+        else
+        {
+            last = targ.transform.position + Vector3.up + offset;
+            GetComponent<Rigidbody>().velocity = (Vector3.Normalize(last - transform.position)) * speed * localTime/5;
+            burst -= Time.deltaTime * localTime;
+        }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && localTime > 0)
+        {
+            Debug.Log("<color=red>Dead</color>");
+        }
+        else if (collision.gameObject != parent && localTime > 0)
+        {
+            Debug.Log("<color=yellow>Destroyed</color>");
+            Destroy(gameObject);
+        }
     }
 }
